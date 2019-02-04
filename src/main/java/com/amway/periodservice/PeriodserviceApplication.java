a719ee4de6867560b1a48fd7168dcee2b1a96e19
Sreenivasa Gopalam <srgopalam@gmail.com>
2019-02-03 21:47:53 -0500

    Initial Commit

---
+++ src/main/java/com/amway/periodservice/PeriodserviceApplication.java
@@ -0,0 +1,12 @@
+package com.amway.periodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodserviceApplication.class, args);
+	}
+}
